99bac1a61b30ae251055056c2284d5b1b1178e1f
tiagonm <tiago.nascimento@vtex.com.br>
2013-04-10 22:02:51 -0300

    jkjk

--- src/FrequenciaDocente/Relatorio/RelatorioMarcacaoDocente.aspx.cs
+++ src/FrequenciaDocente/Relatorio/RelatorioMarcacaoDocente.aspx.cs
@@ -10,11 +10,17 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using Microsoft.Reporting.WebForms;
 
 public partial class FrequenciaDocente_Relatorio_RelatorioMarcacaoDocente : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
         Session["c_Matricula"] = "14134";
+        Session["c_Nome"] = "Fulano de tal";
+        var parametros = new ReportParameter[1];
+        var parametroNome = new ReportParameter("nome",Session["c_Nome"].ToString());
+        parametros[0] = parametroNome;
+        ReportViewer1.LocalReport.SetParameters(parametros);
     }
 }
